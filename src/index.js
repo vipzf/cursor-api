0499f65ce563815f95f8942b4872e6d956329314
Shaun <49644668+vipzf@users.noreply.github.com>
2025-01-12 01:40:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ const { stringToHex, chunkToUtf8String, getRandomIDPro, generateCursorChecksum }
 const app = express();
 
 // 中间件配置
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//噢噢这个啊，200k是另一个配置，这几个cursor-api都没有默认支持，需要自己手动改
+//说实话，我有点怀疑这个906051999/cursor-api的checksum会不会有问题
+//你把自己的index.js的第六行改成这样子，就可以支持了
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 function generateChecksum(token) {
